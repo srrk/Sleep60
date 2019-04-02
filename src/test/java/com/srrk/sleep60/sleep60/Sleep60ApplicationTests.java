9ef8dd559831abee107a29ed6768a8e00ce68594
SrRajeshKumar <rajeshux@gmail.com>
2019-04-02 16:33:25 +0530

    Static Home Page Added

---
+++ src/test/java/com/srrk/sleep60/sleep60/Sleep60ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.srrk.sleep60.sleep60;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sleep60ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
